d2805a9627c955965e95cbdcf55fcc8754a0d90e
Prasannanjaneya <Prasannanjaneya.r@gmail.com>
2025-07-30 19:46:19 +0200

    added files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js App!');
+  res.send('CI/CD integrated Node.js app with AKS deployment');
 });
 
 app.listen(port, () => {
